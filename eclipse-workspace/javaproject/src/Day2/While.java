e22d574db359fa95310e4781fc3d6ddc2ff411e4
unknown <vijaykavitha420@gmail.com>
2023-08-07 11:58:30 +0530

    vijay

---
+++ eclipse-workspace/javaproject/src/Day2/While.java
@@ -0,0 +1,25 @@
+package Day2;
+
+public class While {
+
+	public static void main(String[] args) {
+    int count=0;
+    while(count<5) {
+    	System.out.println("count is"+count);
+    }
+	}
+    	
+    int system=1;
+    int no_of_system=5;
+    do{
+    	system.out.println("no of system "+ system);
+    	System++;
+    }
+    	
+    	
+    }
+    	
+    }
+    		
+
+
---
+++ eclipse-workspace/javaproject/src/javaproject/Door.java
@@ -0,0 +1,36 @@
+package javaproject;
+
+public class Door {
+
+	public static void main(String[] args)
+	{
+		
+		Door d= new Door();
+		d.push();
+		d.pull();
+		/
+		
+        
+		  System.out.println(d.name);
+		}
+	String name="push";
+	
+	public void push()
+	
+	{
+		
+	System.out.println("pull");
+	}
+	public void pull()
+	
+	{
+	
+	System.out.println("color");
+	
+	}
+}
+
+
+
+
+
