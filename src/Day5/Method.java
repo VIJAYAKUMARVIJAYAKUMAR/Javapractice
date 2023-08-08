be9d03cc011da0104944888cc0bd11c1b861a947
unknown <vijaykavitha420@gmail.com>
2023-08-08 13:05:32 +0530

    pushingmethods

---
+++ src/Day5/Method.java
@@ -0,0 +1,18 @@
+package Day5;
+
+public class Method {
+		
+		public static void main (String[]args){
+			int result=addnumbers(4 ,10);
+			System.out.println("the result is" + result); 
+		}
+		public static int addnumbers(int num1,int num2){
+			int sum= num1+num2;
+			return sum;
+
+		}
+			
+			
+		}
+
+
